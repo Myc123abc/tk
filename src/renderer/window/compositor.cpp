6abb76fd0b933579894bbb74d523f3ea66ed5f18
myc <1351579650@qq.com>
2026-04-03 21:08:12 +0800

    use blur backdrop brush

--- src/renderer/window/compositor.cpp
+++ src/renderer/window/compositor.cpp
@@ -1,5 +1,6 @@
 #include "compositor.hpp"
 #include "util/error_handling.hpp"
+#include "effect/effect.h"
 
 namespace {
 
@@ -33,6 +34,10 @@ void Compositor::init() noexcept
   _compositor = winrt::Windows::UI::Composition::Compositor{};
 }
 
+winrt::Windows::UI::Composition::CompositionBrush CreateSimpleBackdropBlurBrush(
+  winrt::Windows::UI::Composition::Compositor const& compositor,
+  float blurAmount = 30.0f);
+
 auto Compositor::create_resource(HWND handle) const noexcept -> Resource
 {
   auto res = Resource{};
@@ -48,10 +53,60 @@ auto Compositor::create_resource(HWND handle) const noexcept -> Resource
   // create blur visual
   res.blur_visual = _compositor.CreateSpriteVisual();
   res.blur_visual.RelativeSizeAdjustment({ 1.f, 1.f });
-  res.blur_visual.Brush(_compositor.CreateColorBrush(winrt::Windows::UI::Colors::Red()));
+  res.blur_visual.Brush(CreateSimpleBackdropBlurBrush(_compositor));
+  // res.blur_visual.Brush(_compositor.CreateColorBrush(winrt::Windows::UI::Colors::Red()));
   res.root.Children().InsertAtBottom(res.blur_visual);
 
   return res;
+}
+
+ComPtr<ICompositionEffectBrush> CreateEffectBrush(
+  winrt::Windows::UI::Composition::Compositor const& compositor,
+  ABI::Windows::Graphics::Effects::IGraphicsEffect* graphicsEffect)
+{
+	ComPtr<ICompositionEffectBrush> effectBrush{nullptr};
+  auto projectedEffect = winrt::Windows::Graphics::Effects::IGraphicsEffect{ nullptr };
+  winrt::copy_from_abi(projectedEffect, graphicsEffect);
+  auto effectFactory = compositor.CreateEffectFactory(projectedEffect).as<ICompositionEffectFactory>();
+	err_if(effectFactory->CreateBrush(&effectBrush), "failed to create effect brush");
+	return effectBrush;
+}
+
+winrt::Windows::UI::Composition::CompositionBrush CreateSimpleBackdropBlurBrush(
+  winrt::Windows::UI::Composition::Compositor const& compositor,
+  float blurAmount)
+{
+  auto backdropFallbackEffect{Make<CompositeStepEffect>()};
+	backdropFallbackEffect->SetCompositeMode(D2D1_COMPOSITE_MODE_SOURCE_OVER);
+	backdropFallbackEffect->SetDestination(CompositionEffectSource(HStringReference(L"WallpaperBackdrop").Get()));
+	backdropFallbackEffect->SetSource(CompositionEffectSource(HStringReference(L"Backdrop").Get()));
+
+	auto blurEffect{ Make<GaussianBlurEffect>() };
+	blurEffect->put_Name(HStringReference(L"Blur").Get());
+	blurEffect->SetBlurAmount(blurAmount);
+	blurEffect->SetBorderMode(D2D1_BORDER_MODE_HARD);
+  blurEffect->SetInput(backdropFallbackEffect.Get());
+
+	ComPtr<ICompositionEffectBrush> effectBrush{ CreateEffectBrush(compositor, blurEffect.Get()) };
+
+  auto backdropBrush = compositor.CreateBackdropBrush();
+	ThrowIfFailed(
+		effectBrush->SetSourceParameter(
+			HStringReference(L"Backdrop").Get(),
+      backdropBrush.as<ICompositionBrush>().get()
+		)
+	);
+
+	ThrowIfFailed(
+		effectBrush->SetSourceParameter(
+			HStringReference(L"WallpaperBackdrop").Get(),
+			backdropBrush.as<ICompositionBrush>().get()
+		)
+	);
+
+  auto projectedBrush = winrt::Windows::UI::Composition::CompositionBrush{ nullptr };
+  winrt::copy_from_abi(projectedBrush, effectBrush.Get());
+  return projectedBrush;
 }
 
 }
